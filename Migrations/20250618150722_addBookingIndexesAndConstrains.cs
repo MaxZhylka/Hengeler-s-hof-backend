08a27def56ef57e63e0c14324f2323e33fbc9cd7
MaxZhylka <dexhonesta@gmail.com>
2025-06-18 18:08:00 +0300

    add migration

---
+++ Migrations/20250618150722_addBookingIndexesAndConstrains.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hengeler.Migrations
+{
+    /// <inheritdoc />
+    public partial class addBookingIndexesAndConstrains : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MoreThanTwoPats",
+                table: "Bookings",
+                newName: "MoreThanTwoPets");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RoomId",
+                table: "Bookings",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_RoomId",
+                table: "Bookings",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_StartDate_EndDate",
+                table: "Bookings",
+                columns: new[] { "StartDate", "EndDate" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_UserId",
+                table: "Bookings",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Users_UserId",
+                table: "Bookings",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Users_UserId",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_RoomId",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_StartDate_EndDate",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_UserId",
+                table: "Bookings");
+
+            migrationBuilder.RenameColumn(
+                name: "MoreThanTwoPets",
+                table: "Bookings",
+                newName: "MoreThanTwoPats");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RoomId",
+                table: "Bookings",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
